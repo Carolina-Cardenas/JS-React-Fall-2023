e6b7a088d1e391e821ad11a30270722f14ca24f5
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2023-11-07 10:41:58 +0100

    firt step

--- weeks/sprint-2/week-6/class-examples/src/App.jsx
+++ weeks/sprint-2/week-6/class-examples/src/App.jsx
@@ -1,12 +1,13 @@
 import { Example } from "./components/Examples/Example";
+import { DogFactOne} from "./components/DogFact/DogFactOne"
 
 export const App = () => {
   return (
     <>
       <h1>⚛ Learning the useEffect() Hook in React ⚛</h1>
       <div className="card">
-        <Example />
-        {/* <DogFactOne /> */}
+        {/* <Example /> */}
+         <DogFactOne /> 
         {/*   <ExampleFour /> */}
       </div>
     </>
